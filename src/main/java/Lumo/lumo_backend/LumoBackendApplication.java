6d2dc734733f0e109a3ec65c3ab7844b0272afdf
jijysun <jijysun@naver.com>
2025-12-23 23:10:39 +0900

    [FEAT] : project init

---
+++ src/main/java/Lumo/lumo_backend/LumoBackendApplication.java
@@ -0,0 +1,13 @@
+package Lumo.lumo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Lumo/lumo_backend/LumoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Lumo.lumo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
