ea5bc851d15cf4144f37c01f2100d3ed14b30ced
minseo6753 <minseo6753@naver.com>
2025-12-26 01:51:31 +0900

    [FEAT] : 메인페이지 엔티티 생성

---
+++ src/main/java/Lumo/lumo_backend/domain/member/entity/Member.java
@@ -0,0 +1,28 @@
+package Lumo.lumo_backend.domain.member.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name = "member")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+}
