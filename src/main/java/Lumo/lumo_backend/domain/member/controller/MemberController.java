6ec0cbbe3f9697858a4569c0ac4717992206c1c6
SH K <jijysun@gmail.com>
2025-12-28 17:24:29 +0900

    [FEAT] : controller 껍데기 작성

--- src/main/java/Lumo/lumo_backend/domain/member/controller/MemberController.java
+++ src/main/java/Lumo/lumo_backend/domain/member/controller/MemberController.java
@@ -1,8 +1,8 @@
 package Lumo.lumo_backend.domain.member.controller;
 
+import Lumo.lumo_backend.global.apiResponse.APIResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -10,6 +10,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
 
+    ///  자동 로그인, 이메일 기억하기 기능도 추가가 필요!
+
+    @GetMapping("/login")
+    public APIResponse<Object> getLoginMethod (){
+        return null; // 로그인 방식 리턴
+    }
+
+    @PostMapping("/logout")
+    public APIResponse<Object> logout (){
+        return null; // bool 값 리턴
+    }
+
+    @PostMapping("/withdrawl")
+    public APIResponse<Object> withdrawal(){
+        return null; // bool 값 리턴, SOFT DELETE
+    }
+
+    @PostMapping("/signin")
+    public APIResponse<Object> signIn(@RequestBody ){
+        return null; // bool 값 리턴,
+    }
 
 
 }
---
+++ src/main/java/Lumo/lumo_backend/domain/member/service/UserService.java
@@ -0,0 +1,9 @@
+package Lumo.lumo_backend.domain.member.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+}
