0e45875f7cdcd1ea5cf5ffee63ddb7291f0f06b3
jijysun <jijysun@naver.com>
2026-01-05 23:04:37 +0900

    [FEAT] : subroutine service, repository 작성

---
+++ src/main/java/Lumo/lumo_backend/domain/subroutine/service/SubroutineService.java
@@ -0,0 +1,17 @@
+package Lumo.lumo_backend.domain.subroutine.service;
+
+import Lumo.lumo_backend.domain.member.entity.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class SubroutineService {
+
+    public void createSubroutine (Member member, Long routineId, String title) {
+
+
+    }
+}
