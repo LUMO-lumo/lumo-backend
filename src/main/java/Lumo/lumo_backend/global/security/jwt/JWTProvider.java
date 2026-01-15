f43cf60e7559da574816a8ab1bdfb23a7521df3d
jijysun <jijysun@naver.com>
2026-01-15 18:12:18 +0900

    [FEAT] : JWT - getAuthtication 로직 구현 내부 예외 처리는 일단 RuntimeException 예외로 처리, 이후 GeneralException + ErrorCode 로 처리 예정

--- src/main/java/Lumo/lumo_backend/global/security/jwt/JWTProvider.java
+++ src/main/java/Lumo/lumo_backend/global/security/jwt/JWTProvider.java
@@ -3,11 +3,12 @@ package Lumo.lumo_backend.global.security.jwt;
 import Lumo.lumo_backend.global.security.userDetails.CustomUserDetailsService;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.SecurityException;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import io.jsonwebtoken.*;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -28,7 +29,6 @@ public class JWTProvider {
     }
 
     public boolean validateToken(String accessToken) {
-
         try{
             Jwts.parser()
                     .verifyWith((SecretKey) key)
@@ -44,13 +44,44 @@ public class JWTProvider {
         }
         catch (UnsupportedJwtException e) {
             log.warn("[JWTProvider-ValidateToken()] : 지원되지 않는 토큰 형식입니다 ", e);
-        } catch (IllegalArgumentException e) {
+        }
+        catch (IllegalArgumentException e) {
             log.warn("[JWTProvider-ValidateToken()] : JWT claims String이 비어있습니다. ", e);
         }
         return false;
     }
 
     public Authentication getAuthentication(String accessToken) {
-        return null;
+        Claims claims = parseClaims(accessToken);
+
+        Object authClaimObject = claims.get("auth") == null ? claims.get("auth") : "";
+        String authoritiesString = (authClaimObject != null) ? authClaimObject.toString() : "";
+
+        if (authoritiesString.isEmpty() || claims.get("auth") == null) {
+            ///  GenerationException 으로 수정하기
+            throw new RuntimeException("권한 정보가 없는 이상한 토큰입니다");
+        }
+
+        log.info("[JWTProvider - getAuthentication()] email: {}", claims.get("username", String.class));
+
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(claims.get("username", String.class));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    private Claims parseClaims(String accessToken) {
+        try{
+            Claims claims = Jwts.parser()
+                    .verifyWith((SecretKey) key)
+                    .build()
+                    .parseSignedClaims(accessToken)
+                    .getPayload();
+
+            log.info ("[JWTProvider - parseClaims()] claims 파싱 발생 -> {}, {}", claims.toString(), claims.getSubject());
+            return claims;
+        }
+        catch (Exception e){
+            ///  GenerationException 으로 수정하기
+            throw new RuntimeException("파싱이 잘못되었습니다.");
+        }
     }
 }
