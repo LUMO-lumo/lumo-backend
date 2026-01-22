5329d832a4e679b469c492640f49e9c607b264c1
minseo6753 <minseo6753@naver.com>
2026-01-23 01:40:30 +0900

    [FEAT] : 공지사항 생성 API 구현

--- src/main/java/Lumo/lumo_backend/domain/setting/service/NoticeService.java
+++ src/main/java/Lumo/lumo_backend/domain/setting/service/NoticeService.java
@@ -1,4 +1,31 @@
 package Lumo.lumo_backend.domain.setting.service;
 
+import Lumo.lumo_backend.domain.setting.dto.NoticeCreateRequestDTO;
+import Lumo.lumo_backend.domain.setting.dto.NoticeResponseDTO;
+import Lumo.lumo_backend.domain.setting.entity.Notice;
+import Lumo.lumo_backend.domain.setting.repository.NoticeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class NoticeService {
+    private final NoticeRepository noticeRepository;
+
+    public NoticeResponseDTO create(NoticeCreateRequestDTO noticeCreateRequestDTO) {
+
+        Notice notice = Notice.builder()
+                .type(noticeCreateRequestDTO.getType())
+                .title(noticeCreateRequestDTO.getTitle())
+                .content(noticeCreateRequestDTO.getContent())
+                .isActive(true)
+                .build();
+
+        Notice saved = noticeRepository.save(notice);
+        return NoticeResponseDTO.from(saved);
+    }
+
+
 }
