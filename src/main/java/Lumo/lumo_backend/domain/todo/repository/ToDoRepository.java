37ceb7ced018b0bb1be134acb75bed04fcaac5ee
minseo6753 <minseo6753@naver.com>
2026-01-27 04:23:18 +0900

    [REFAC] : 할 일 응답 DTO 변경

--- src/main/java/Lumo/lumo_backend/domain/todo/repository/ToDoRepository.java
+++ src/main/java/Lumo/lumo_backend/domain/todo/repository/ToDoRepository.java
@@ -1,11 +1,12 @@
 package Lumo.lumo_backend.domain.todo.repository;
 
 import Lumo.lumo_backend.domain.member.entity.Member;
+import Lumo.lumo_backend.domain.todo.dto.response.ToDoResponseDTO;
 import Lumo.lumo_backend.domain.todo.entity.ToDo;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ToDoRepository extends JpaRepository<ToDo, Long> {
-    List<ToDo> findAllByMemberAndEventDate(Member member, LocalDate eventDate);
+    List<ToDoResponseDTO> findAllByMemberAndEventDate(Member member, LocalDate eventDate);
 }
