935c76cbfd54bfa8d61a2c849c92d6969c1514d6
jijysun <jijysun@naver.com>
2026-02-05 18:43:14 +0900

    [FIX] : 공통 응답 코드 수정

--- src/main/java/Lumo/lumo_backend/domain/member/status/MemberErrorCode.java
+++ src/main/java/Lumo/lumo_backend/domain/member/status/MemberErrorCode.java
@@ -15,13 +15,13 @@ public enum MemberErrorCode implements BaseErrorCode {
     TEST_EXCEPTION (HttpStatus.BAD_REQUEST, "TEST4000", "테스트 예외 입니다."),
 
     // 여기서부터 이어서 작성해주시기 바랍니다.
-    CANT_FOUND_MEMBER(HttpStatus.BAD_REQUEST, "MEMBER4000", "알 수 없는 사용자입니다."),
-    WRONG_CODE(HttpStatus.BAD_REQUEST, "MEMBER4001", "옳지 않은 인증 코드 입니다."),
-    EXIST_MEMBER(HttpStatus.BAD_REQUEST, "MEMBER4002", "이미 존재하는 회원입니다."),
-    ALREADY_SEND(HttpStatus.BAD_REQUEST, "MEMBER4003", "이미 이메일을 보냈습니다, 3분 뒤 다시 요청해주세요."),
+    CANT_FOUND_MEMBER(HttpStatus.BAD_REQUEST, "MEMBER_4000", "알 수 없는 사용자입니다."),
+    WRONG_CODE(HttpStatus.BAD_REQUEST, "MEMBER_4001", "옳지 않은 인증 코드 입니다."),
+    EXIST_MEMBER(HttpStatus.BAD_REQUEST, "MEMBER_4002", "이미 존재하는 회원입니다."),
+    ALREADY_SEND(HttpStatus.BAD_REQUEST, "MEMBER_4003", "이미 이메일을 보냈습니다, 3분 뒤 다시 요청해주세요."),
 
 
-    CANT_SEND_EMAIL (HttpStatus.INTERNAL_SERVER_ERROR, "MEMBER4001", "해당 이메일로 메일을 보낼 수 없습니다. 관리자에게 연락해주시기 바랍니다")
+    CANT_SEND_EMAIL (HttpStatus.INTERNAL_SERVER_ERROR, "MEMBER_5001", "해당 이메일로 메일을 보낼 수 없습니다. 관리자에게 연락해주시기 바랍니다")
     ;
 
 
