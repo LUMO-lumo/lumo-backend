28c1d6b8a60aea652933295b8f9d7bb3f0ca4ddd
jijysun <jijysun@gmail.com>
2026-02-15 17:23:32 +0900

    [FIX] : Bean Autowired 해결

--- src/main/java/Lumo/lumo_backend/global/security/filter/JWTAuthenticationFilter.java
+++ src/main/java/Lumo/lumo_backend/global/security/filter/JWTAuthenticationFilter.java
@@ -22,6 +22,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -29,6 +30,7 @@ import java.util.Arrays;
 
 @Slf4j
 @RequiredArgsConstructor
+@Component
 public class JWTAuthenticationFilter extends OncePerRequestFilter {
 
     private final JWTProvider jwtProvider;
